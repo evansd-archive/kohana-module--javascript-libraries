377c945d4ec876cbbcb6a521203203c87057acc6
Dave <dave@s3.swerveconcepts.com>
2008-09-29 18:36:54 +0100

    added one more plugin

---
+++ javascript/mootools/plugins/Fx.Scroll.showElement.js
@@ -0,0 +1,29 @@
+<?php
+	$this->requires('mootools/Fx.Scroll.js');
+?>
+
+Fx.Scroll.implement({
+	
+	/*
+		Scroll container the minimum amount necessary to ensure that the passed in element is fully visible
+	*/
+	
+	showElement: function(el){
+		var pos = $(el).getCoordinates(this.element);
+		var scroll = this.element.getScroll();
+		
+		if(pos.top < scroll.y) {
+			return this.cancel().start(scroll.x, pos.top);
+		}
+		
+		var size = this.element.getSize();
+		
+		if(pos.bottom > size.y + scroll.y) {
+			return this.cancel().start(scroll.x, pos.bottom - size.y);
+		}
+		
+		return this;
+		
+	}
+	
+});
